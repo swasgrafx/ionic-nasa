c512d1a3d9d3e2a0f125a2c6f8a658a685cd0d39
CHAD-SVASTISALEE <swasgrafx@gmail.com>
2018-09-25 10:58:02 -0500

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,24 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { HttpClientModule } from '@angular/common/http';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { ApodProvider } from '../providers/apod/apod';
+
+import { SafePipe } from '../safe-pipe'
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    SafePipe
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpClientModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,7 +30,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ApodProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,55 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { ApodProvider } from '../../providers/apod/apod';
+import { Apod } from '../../apod';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  apod: Apod;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(
+    public navCtrl: NavController,
+    private apodProvider: ApodProvider
+    ){
+  }
+  ngOnInit() {
+    this.getApod();
   }
 
+  randomDate(start, end){
+    let date = new Date(
+      start.getTime() + Math.random() * (end.getTime() - start.getTime())
+    );
+
+    let d:any = date.getDate();
+    let m:any = date.getMonth() + 1;
+    let y:any = date.getFullYear();
+
+    if(m<10){
+      m = '0'+m;
+    }
+
+    if(d<10){
+      d = '0'+d;
+    }
+
+    return y + '-' + m + '-' + d;
+  }
+
+  getApod(date:string=''):void {
+    this.apodProvider.getApod(date)
+      .subscribe(apod => this.apod = apod);
+  }
+
+  randomApod(): void{
+    let date = this.randomDate(
+      new Date(1995,5,16),
+      new Date()
+    );
+    this.getApod(date);
+  }
 }
